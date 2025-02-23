61d3ed864facb1ae745c949e34a30b3d6d087caa
Trần Việt Anh <tranvietanh0507@gmail.com>
2025-02-23 21:51:49 +0700

    ltm

---
+++ CTDL_005.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++){
+        int x; cin >> x; v.push_back(x);
+    }
+    int k; cin >> k;
+    for (int i = 0; i < n; i++){
+        if(v[i] == k) continue;
+        cout << v[i] << " ";
+    }
+    return 0;
+}
